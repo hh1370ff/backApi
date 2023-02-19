b79a166977cee9ef82320280ebca8aff0246dcf8
Hussein <Husseinfarsangi1370@gmail.com>
2023-02-19 08:18:04 +0330

    last

--- config/redis.js
+++ config/redis.js
@@ -1,6 +1,6 @@
 import redis from "redis";
 // process.env.REDIS_URL
-const client = redis.createClient(process.env.REDIS_URL_EXTERNAL);
+const client = redis.createClient(process.env.REDIS_URL);
 client.on("connect", () => console.log("Connected to Redis"));
 client.on("error", (err) => console.error(err));
 
